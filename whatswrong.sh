2e5b78db3b61033e8652abc0c1eb09bf54f43953
isti varga <131890314+asciikatze@users.noreply.github.com>
2024-11-18 12:30:45 +0100

    Update whatswrong.sh
    added battery indicator

--- whatswrong.sh
+++ whatswrong.sh
@@ -4,12 +4,14 @@ watch '
 if [ -f /var/run/reboot-required ]; then
 	cat /var/run/reboot-required
 	cat /var/run/reboot-required.pkgs
-	echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+	echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 fi
 cat /proc/meminfo | grep "MemAvailable";
 cat /proc/loadavg;
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 cat /proc/cpuinfo | grep "cpu MHz";
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-sensors | grep -E "Package id|Core|temp1|fan"
+echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+sensors | grep -E "Core|fan"
+echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "energy:|energy-full:"
 '
